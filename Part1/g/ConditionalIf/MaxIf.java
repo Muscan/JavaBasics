9551d2275a6a16b9af08b07bed291265a1429983
Mihai <kubikweg@gmail.com>
2021-04-23 12:16:01 +0300

    If

---
+++ Part1/g/ConditionalIf/MaxIf.java
@@ -0,0 +1,50 @@
+package g.ConditionalIf;
+
+import java.util.Scanner;
+
+public class MaxIf {
+    public static void main(String[] args) {
+
+        //todo:Add 2 numbers.Print the max
+        Scanner readScanner = new Scanner(System.in);
+        System.out.println("Insert first nr.");
+        int x = readScanner.nextInt();
+        System.out.println("Insert second nr.");
+        int y = readScanner.nextInt();
+        //todo:if
+//        if (x > y){
+//
+//            System.out.println("Max is "+x);
+//        }
+//        if (x < y){
+//            System.out.println("MMax is" + y);
+//        }
+//        if (x==y){
+//            System.out.println("Numbers are equal");
+//        }
+
+        //todo:if ele
+/*
+
+       if(x > y){
+           System.out.println("Max is  " + x);
+           } else {
+           System.out.println("Max is " + y);
+       }*/
+        //todo: if else var 2.
+
+        if (x > y){
+            System.out.println("Max is " + x);
+        } else if (x < y){
+            System.out.println("Max is  " + y);
+        } else if (x == y){
+            System.out.println("Numbers are equal");
+        } else if (x != 0){
+            System.out.println("Inserted number " +  x + "is != 0");
+        }
+        else if (y ==0){
+            System.out.println("Inserted number " +  y + " is != 0");
+        }
+    }
+
+}
